71933c9993aa00f04f976b67208c246d26696ba6
zerok06 <jose.geeksjose@gmail.com>
2024-03-10 22:30:43 -0500

    add: Implementacion de about conference

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(var(--primary))',
+        bg: 'hsl(var(--bg))',
+      },
+    },
   },
   plugins: [],
 }
